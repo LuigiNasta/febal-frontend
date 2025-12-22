47f3b6aafdd7942d03e6f041f1bd4dad1fc6034e
Luigi Nasta <luiginasta01@gmail.com>
2025-12-22 14:06:40 +0100

    Adjustment 2 Offerte

--- app/offerte/page.tsx
+++ app/offerte/page.tsx
@@ -3,7 +3,7 @@ import Products from "../components/Products";
 
 export default function OffertePage() {
   return (
-    <main className="min-h-screen bg-gray-50 pt-32">
+    <main className="min-h-screen bg-gray-50">
       <section className="bg-red-800 text-white py-25 text-center">
         <h1 className="text-5xl font-bold">OFFERTE SPECIALI</h1>
         <p className="mt-4 text-xl">Articoli invenduti nuovi a prezzi incredibili!</p>
